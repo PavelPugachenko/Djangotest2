ebbe4d829cf9d7572f2bff9b46389abca1776dca
pavelpugacenko <pugachenkopavel72@gmail.com>
2024-12-09 22:11:51 +0500

    commit test

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/'), include('students.urls')
+    path('', include('students.urls', namespace='students')),
 ]
--- students/urls.py
+++ students/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from setuptools.extern import names
+from students import views
 
-from  . import views
+app_name = 'students'
 
 urlpatterns = [
-    path('show_data/', views.show_data, name='show_data'),
-    path('submit_data/', views.submit_data, name='submit_data'),
-    path('item/<int:item_id>', views.show_item, name='show_item')
+    path('about/', views.about, name='about'),
 ]
