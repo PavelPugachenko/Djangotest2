3c3f693deaaa7fac51ebb70741e6239602d851cc
pavelpugacenko <pugachenkopavel72@gmail.com>
2024-12-09 21:31:23 +0500

    commit test

--- students/views.py
+++ students/views.py
@@ -12,3 +12,7 @@ def submit_data(request):
     if request.method == 'POST':
         # Обработка данных формы
         return HttpResponse("Данные отправлены!")
+
+def show_item(request, item_id):
+    # Логика для обработки данных элемента с указанным ID
+    return render(request, 'app/item.html', {'item_id': item_id})
