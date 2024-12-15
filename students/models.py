e68c4be1ad9b9c7674defa0d37dc700ecc0afbdb
pavel <pugachenkopavel72@gmail.com>
2024-12-15 15:53:44 +0500

    23212

--- students/models.py
+++ students/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
-    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+class Group(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return self.name
 
-    class Meta:
-        verbose_name = 'студент'
-        verbose_name_plural = 'студенты'
-        ordering = ['last_name']
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='students')
+
+    def __str__(self):
+        return self.name
